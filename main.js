65895761a17addd16e84c24b8622afb4c4b6efc3
roba <10408936+roba-adnew@users.noreply.github.com>
2023-10-02 16:42:27 -0400

    Added a delete button
    Added a delete button to enable the user to delete the book from their
    library and from the display.
    
     Changes to be committed:
    	modified:   index.html
    	modified:   main.js

--- main.js
+++ main.js
@@ -12,65 +12,56 @@ function Book(title, author, pages, read) {
     }
 }
 
-function addNewBookForm() {
-    const newBookButton = document.getElementById('newbook');
+function createNewBookForm() {
     
-    newBookButton.addEventListener('click', function() {
-        function createForm() {
-            const newBookForm = document.createElement('form');
-            newBookForm.setAttribute('id','form');
-            newBookForm.style.cssText = 
-            'display: flex; flex-direction: column; flex-basis: 50px; margin: 10px' ;
-            document.body.appendChild(newBookForm);
+    
+    const newBookForm = document.createElement('form');
+    newBookForm.setAttribute('id','form');
+    newBookForm.style.cssText = 
+        'display: flex; flex-direction: column; flex-basis: 50px; margin: 10px' ;
+    newBookButton.after(newBookForm);
 
-            let bookExample = new Book();
-            for (key in bookExample) {
-                if (key != 'info') {
-                    const newLabel = document.createElement('label');
-                    newLabel.style.cssText = 'width: 100px';
-                    newLabel.innerHTML = key;
-                    newBookForm.appendChild(newLabel);
-                    
-                    const newField = document.createElement('input');
-                    newField.setAttribute('id', key);
-                    newField.style.cssText = 'width: 300px;'
-                    newBookForm.appendChild(newField);
-                } 
-            } 
+    let bookExample = new Book();
+    for (key in bookExample) {
+        if (key != 'info') {
+            const newLabel = document.createElement('label');
+            newLabel.style.cssText = 'width: 100px';
+            newLabel.innerHTML = key;
+            newBookForm.appendChild(newLabel);
+            
+            const newField = document.createElement('input');
+            newField.setAttribute('id', key);
+            newField.style.cssText = 'width: 300px;'
+            newBookForm.appendChild(newField);
+        } 
+    } 
 
-            const submitButton = document.createElement('button');
-            submitButton.setAttribute('id', 'submit-newbook');
-            submitButton.style.cssText = 'width: 300px; margin: 5px'
-            submitButton.innerHTML = 'Add this book';
-            newBookForm.appendChild(submitButton);
-            return
-        }
-        createForm();
-
-        function submitBook() {
-            newBookForm = document.getElementById('form');
-            submitButton = document.getElementById('submit-newbook');
-            submitButton.addEventListener('click', function(event) {
-                event.preventDefault();
-                const newBook = new Book(
-                    newBookForm.Title.value, 
-                    newBookForm.Author.value,
-                    newBookForm .Pages.value, 
-                    newBookForm.Read.value
-                )
-                myLibrary.push(newBook);
-                newBookForm.replaceChildren();
-
-                // createNewTable();
-                clearLibraryTable();
-                showCaseLibrary();
-            })
-        }
-        submitBook();
-    })
+    const submitButton = document.createElement('button');
+    submitButton.setAttribute('id', 'submit-newbook');
+    submitButton.style.cssText = 'width: 300px; margin: 5px'
+    submitButton.innerHTML = 'Add this book';
+    newBookForm.appendChild(submitButton);
 }
 
-function createNewTable() { 
+function clearLibraryTable() {
+    const currentTable = document.querySelector('table');
+    if (currentTable) {
+        currentTable.replaceChildren();
+        currentTable.remove();
+    }
+    
+};
+
+function clearNewBookForm() {
+    const currentForm = document.querySelector('form');
+    if (currentForm) {
+        currentForm.replaceChildren();
+        currentForm.remove();
+    };
+};
+
+function showCaseLibrary() {
+
     const table =  document.createElement('table');
     table.setAttribute('id', 'library');
     document.body.appendChild(table);
@@ -94,31 +85,66 @@ function createNewTable() {
     const readHeader = document.createElement('th');
     readHeader.innerHTML = 'Read';
     header.appendChild(readHeader);
-}
 
-function clearLibraryTable() {
-    const currentTable = document.querySelector('table');
-    currentTable.replaceChildren();
-};
+    const libraryLength = myLibrary.length;
 
-function showCaseLibrary() {
+    for (let i = 0; i < libraryLength; i++) {
 
-    const currentTable = document.querySelector('table');
-
-    for (let i = 0; i < myLibrary.length; i++) {
         const newRow = document.createElement('tr');
-        currentTable.append(newRow)
-
+        
         for (const key in myLibrary[i]) {
             if (key != 'info') {
                 const newCell = document.createElement('td');
-                newCell.innerHTML = myLibrary[i]    [key];
-                newRow.append(newCell);
+                newCell.innerHTML = myLibrary[i][key];
+                newRow.appendChild(newCell);
             }
         }
+
+        const deleteButton = document.createElement('button');
+        deleteButton.innerHTML = 'Delete';
+        deleteButton.setAttribute('data-id', i);
+        deleteButton.className = 'delete';
+        
+        newRow.setAttribute('data-id', i);
+        newRow.appendChild(deleteButton);
+        table.appendChild(newRow);
     }
+
+    const deleteButtonCollection = document.getElementsByClassName('delete');
+
+    for (let button of deleteButtonCollection) {
+        button.addEventListener('click',function(event) {
+            const rowToDelete = document.querySelector(`tr[data-id="${button.dataset.id}"]`);
+            myLibrary.splice(button.dataset.id,1);
+            rowToDelete.replaceChildren();
+            rowToDelete.remove();
+        })
+    }  
 }
 
-createNewTable();
-addNewBookForm();
-showCaseLibrary();
+function submitNewBook() {  
+    newBookForm = document.getElementById('form');
+
+    const newBook = new Book(
+        newBookForm.Title.value, 
+        newBookForm.Author.value,
+        newBookForm.Pages.value, 
+        newBookForm.Read.value
+    )
+    myLibrary.push(newBook);
+        
+    clearNewBookForm();
+    clearLibraryTable();
+    showCaseLibrary();
+};
+
+const newBookButton = document.getElementById('new-book-button');
+newBookButton.addEventListener('click', function() {
+    createNewBookForm();
+
+    const submitButton = document.getElementById('submit-newbook');
+    submitButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        submitNewBook();  
+    })
+});
